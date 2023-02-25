12054c28b4ebf6af47b96c67d417f0ed149e73ed
David Ssali <ssalidm@gmail.com>
2023-02-25 12:17:12 +0200

    Add 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _abs - computes the absolute value of an integer.
+*
+* @num: value to calculate
+*
+* Return: Absolute value of n.
+*/
+
+int _abs(int num)
+{
+if (num < 0)
+{
+return (num * -1);
+}
+else
+{
+return (num);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ void print_to_98(int n);
 int add(int a, int b);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int num);
