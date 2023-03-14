1bc1f4ad2e28e43712b1bea961463e89241ef354
David Ssali <ssalidm@gmail.com>
2023-03-14 18:26:46 +0200

    vim

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to the program
+ *
+ * @argc: The number of command-line arguments.
+ * @argv: Pointer to the list of command-line arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
